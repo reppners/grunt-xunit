03ba07b90aaf91a61df4d444907b4ff2b83fdb4e
reppners <stefanreppner@gmail.com>
2016-10-31 17:06:31 +0100

    added xunit 2.1 runner options with documentation option for not aborting grunt when tests fail

--- tasks/xunit.js
+++ tasks/xunit.js
@@ -10,13 +10,67 @@ module.exports = function (grunt) {
     grunt.registerMultiTask('xunit', 'Run xUnit tests.', function () {
         var options = this.options({
                 cwd: '',
+                // specify a runner path
                 runner: path.resolve(__dirname, '../vendor/xunit/xunit.console.exe'),
-                xml: path.join(os.tmpdir(), _.uniqueId('xunit') + '.xml')
+                // specify a path to a xunit config file (xml or json)
+                config: '',
+                // specify a default xml result file for later parsing results in grunt
+                xml: path.join(os.tmpdir(), _.uniqueId('xunit') + '.xml'),
+                // flag for continuing on test failures useful for CI environments
+                continueOnTestfailure: false
             }),
-            reporter = require(options.reporter || '../lib/reporters/spec'),
+
+            // reporter implementation converting the result to grunt
+            reporter = require(options.reporter || '../lib/reporters/spec');
+
             xunitOptions =
                 _.chain(options)
-                .pick(['silent', 'teamcity', 'wait', 'trait', '-trait', 'noshadow', 'xml', 'html', 'nunit'])
+                .pick([
+                    // v2.1.0 runner options
+                    'nologo',       //do not show the copyright message
+                    'nocolor',      //do not output results with colors
+                    'noappdomain',  //do not use app domains to run test code
+                    'failskips',    //convert skipped tests into failures
+                    'parallel',     //set parallelization based on option
+                                    // none        - turn off all parallelization
+                                    // collections - only parallelize collections
+                                    // assemblies  - only parallelize assemblies
+                                    // all         - parallelize assemblies & collections
+                    'maxthreads',   // maximum thread count for collection parallelization
+                                    // default   - run with default (1 thread per CPU thread)
+                                    // unlimited - run with unbounded thread count
+                                    // (number)  - limit task thread pool size to 'count'
+                    'noshadow',     // do not shadow copy assemblies
+                    'wait',         // wait for input after completion
+                    'diagnostics',  // enable diagnostics messages for all test assemblies
+                    'debug',        // launch the debugger to debug the tests
+                    'serialize',    // serialize all test cases (for diagnostic purposes only)
+                    'trait',        // "name=value"  : only run tests with matching name/value traits
+                                    // if specified more than once, acts as an OR operation
+                    'notrait',      // "name=value"  : do not run tests with matching name/value traits
+                                    // if specified more than once, acts as an AND operation
+                    'method',       // run a given test method (should be fully specified;
+                                    // i.e., 'MyNamespace.MyClass.MyTestMethod')
+                                    // if specified more than once, acts as an OR operation
+                    'class',        // run all methods in a given test class (should be fully
+                                    // specified; i.e., 'MyNamespace.MyClass')
+                                    // if specified more than once, acts as an OR operation
+                    'namespace',    // run all methods in a given namespace (i.e.,
+                                    // 'MyNamespace.MySubNamespace')
+                                    // if specified more than once, acts as an OR operation
+
+                    // reporters    (optional, choose only one)
+                    'appveyor',     // forces AppVeyor CI mode (normally auto-detected)
+                    'quiet',        // do not show progress messages
+                    'teamcity',     // forces TeamCity mode (normally auto-detected)
+                    'verbose',      // show verbose progress messages
+
+                    // result formats (optional, choose one or more)
+                    'xml',          // output results to xUnit.net v2 style XML file
+                    'xmlv1',        // output results to xUnit.net v1 style XML file
+                    'nunit',        // output results to NUnit-style XML file
+                    'html'          // output results to HTML file
+                ])
                 .map(function (value, key) {
                     if (typeof value === 'string') {
                         return '-' + key + ' ' + value;
@@ -37,19 +91,37 @@ module.exports = function (grunt) {
 
 
         async.series(this.files.map(function (file) {
-            return function (callback) { test(file.src, callback); };
+
+            return function (callback) {
+
+                // resolve config file path if defined
+                var config;
+                if(options.config) {
+                    config = path.resolve(options.config + '')
+                }
+
+                test(file.src, config, callback);
+            };
         }), this.async());
 
-        function test (file, callback) {
-            var command = [path.resolve(options.runner + ''), path.resolve(file + ''), xunitOptions],
+        function test (file, config, callback) {
+            var command = [
+                    path.resolve(options.runner + ''),
+                    path.resolve(file + ''),
+                    config || '',
+                    xunitOptions
+                ],
                 child;
 
             if (process.platform !== 'win32') {
                 command.unshift('mono');
             }
 
-            grunt.log.debug('Running command: ' + command.join(' '));
-            child = child_process.exec(command.join(' '));
+            command = command.join(' ');
+
+            grunt.log.debug('Running command: ' + command);
+
+            child = child_process.exec(command);
 
             child.stdout.on('data', function (chunk) {
                 grunt.log.debug(chunk.toString('utf8'));
@@ -60,22 +132,26 @@ module.exports = function (grunt) {
             });
 
             child.on('exit', function (code) {
-                fs.readFile(options.xml, function (error, xml) {
-                    if(error) {
-                        grunt.fail.warn(error);
+
+                fs.readFile(options.xml, function (xmlReadError, xml) {
+
+                    if(xmlReadError && !options.continueOnTestfailure) {
+                        grunt.fail.warn('failed to read xunit result xml: ' + xmlReadError);
                     }
 
-                    xml2js.parseString(xml, function (error, result) {
-                        if(error) {
-                            grunt.fail.warn(error);
+                    xml2js.parseString(xml, function (xmlParseError, result) {
+
+                        if(xmlParseError) {
+                            grunt.fail.warn('failed to parse xunit result xml: ' + xmlParseError);
                         }
+
                         reporter(grunt, result);
 
-                        if(code) {
-                            grunt.fail.warn('');
+                        if(code && !options.continueOnTestfailure) {
+                            grunt.fail.warn('xunit runner returned with code ' + code);
                         }
 
-                        callback(null);
+                        callback();
                     });
                 });
             });
